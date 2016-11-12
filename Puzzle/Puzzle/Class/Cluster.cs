d9a692f43d133cf68494152bdcf2591729a5d5e9
Bartosz Sowa <b.sowa9@gmail.com>
2016-11-12 16:47:46 +0100

    Dodanie klas Piece, Cluster, Coordinate

---
+++ Puzzle/Puzzle/Class/Cluster.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace Puzzle.Class
+{
+    /// <summary>
+    /// Klasa reprezentująca grupę złączonych kawałków puzzli
+    /// </summary>
+    internal class Cluster
+    {
+        /// <summary>
+        /// Id klastra (złączonych kawałków puzzli)
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Lista kawałków należących do danego klastra
+        /// </summary>
+        public List<Piece> Pieces { get; set; }
+    }
+}
---
+++ Puzzle/Puzzle/Class/Coordinate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Puzzle
+{
+    internal class Coordinate
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public Coordinate()
+        {
+            
+        }
+
+        public Coordinate(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
