13daf218085b43d47cb65ffff2a6d2e6aa13e4ca
Bartosz Sowa <b.sowa9@gmail.com>
2016-11-19 17:53:20 +0100

    Dodanie modelu PieceCluster (reprezentuje grupę/kilka kawałków puzzli)

---
+++ Puzzle/Puzzle/Class/PieceCluster.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Puzzle.Class
+{
+    /// <summary>
+    /// Klasa reprezentująca grupę złączonych kawałków puzzli
+    /// </summary>
+    internal class Cluster
+    {
+        /// <summary>
+        /// Id klastra (złączonych kawałków puzzli)
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Lista kawałków należących do danego klastra
+        /// </summary>
+        public List<Piece> Pieces { get; set; }
+    }
+}
