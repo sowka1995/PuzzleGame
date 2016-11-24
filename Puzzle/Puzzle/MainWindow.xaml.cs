8b45708497d03c90964d1250c6bdbad673e1742e
Krzysiek <Krzysiek@KRZYSIEKPC>
2016-11-24 19:05:32 +0100

    Dodanie eventów umożliwiających przesuwanie puzzli.

--- Puzzle/Puzzle/MainWindow.xaml.cs
+++ Puzzle/Puzzle/MainWindow.xaml.cs
@@ -13,8 +13,6 @@ using System.Windows.Media.Effects;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.Drawing;
-using System.Drawing.Imaging;
 using Puzzle.Class;
 
 namespace Puzzle
@@ -24,6 +22,30 @@ namespace Puzzle
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region Variables
+
+        /// <summary>
+        /// Określa czy można przesunąć kawałek puzzli lub klaster
+        /// </summary>
+        private bool _canMovePiece;
+
+        /// <summary>
+        /// Przechowuje poprzednią pozycje myszki
+        /// </summary>
+        private Point _previousMousePosition;
+
+        /// <summary>
+        /// Aktualnie przenoszony klaster
+        /// </summary>
+        private Cluster _currentCluster;
+
+        /// <summary>
+        /// Przechowuje wszystkie klastry puzzli
+        /// </summary>
+        private List<Cluster> _clusters;
+
+        #endregion 
+
         public MainWindow()
         {
             InitializeComponent();
@@ -33,5 +55,60 @@ namespace Puzzle
         {
 
         }
+
+        #region Events
+
+        private void PieceCluster_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_canMovePiece && e.LeftButton == MouseButtonState.Pressed)
+            {
+                Point tmpPosition = e.GetPosition(this);
+                Point offset = new Point(_previousMousePosition.X - tmpPosition.X, _previousMousePosition.Y - tmpPosition.Y);
+
+                foreach (Piece piece in _currentCluster.Pieces)
+                {
+                    Canvas.SetLeft(piece.PieceImage, Canvas.GetLeft(piece.PieceImage) - offset.X);
+                    Canvas.SetTop(piece.PieceImage, Canvas.GetTop(piece.PieceImage) - offset.Y);
+                }
+
+                _previousMousePosition = tmpPosition;
+            }
+        }
+
+        private void PieceCluster_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Mouse.OverrideCursor = Cursors.Hand;
+            Image pieceImage = sender as Image;
+
+            int pieceId = int.Parse(pieceImage.Name.Substring(1)); // id z nazwy 
+            _previousMousePosition = e.GetPosition(this);
+            _canMovePiece = true;
+
+            foreach (Cluster cluster in _clusters)
+            {
+                foreach (Piece piece in cluster.Pieces)
+                {
+                    if (piece.Id == pieceId)
+                    {
+                        _currentCluster = cluster;
+                        break;
+                    }
+                }
+            }
+        }
+
+        private void PieceCluster_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            Mouse.OverrideCursor = Cursors.Arrow;
+
+            if (_canMovePiece)
+            {
+                // TODO: łączenie puzzli
+
+                _canMovePiece = false;
+            }
+        }
+
+        #endregion 
     }
 }
