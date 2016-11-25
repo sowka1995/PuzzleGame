bfc67be8a4e2a83eaa065fcbdcfdd1aabf5d3234
energetyk <blackwolf15@o2.pl>
2016-11-25 16:24:14 +0100

    Dodano prototyp menu aplikacji oraz możliwośc wczytywania zdjęć do aplikacji z systemu plików.

---
+++ Puzzle/Puzzle/MenuWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+using Puzzle.Class;
+
+namespace Puzzle
+{
+    /// <summary>
+    /// Interaction logic for MenuWindow.xaml
+    /// </summary>
+    public partial class MenuWindow : Window
+    {
+        public MenuWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog openFileDialog = new Microsoft.Win32.OpenFileDialog();
+            openFileDialog.Filter = "Photo|*.png;*.jpg;*.jpeg";
+
+            Nullable<bool> openFileResult = openFileDialog.ShowDialog();
+
+            if (openFileResult == true)
+            {
+                
+            }
+        }
+    }
+}
